755e8561e1cd19e5bbc1553f4914f5241f0ae227
Crazy_Owl <stalker401@gmail.com>
2012-09-03 00:19:47 +0400

    SRSLY? There is verbose_name for RelatedField, but none for ToOneField? Lol.

--- tastypie/fields.py
+++ tastypie/fields.py
@@ -618,11 +618,11 @@ class ToOneField(RelatedField):
 
     def __init__(self, to, attribute, related_name=None, default=NOT_PROVIDED,
                  null=False, blank=False, readonly=False, full=False,
-                 unique=False, help_text=None):
+                 unique=False, help_text=None, verbose_name=None):
         super(ToOneField, self).__init__(
             to, attribute, related_name=related_name, default=default,
             null=null, blank=blank, readonly=readonly, full=full,
-            unique=unique, help_text=help_text
+            unique=unique, help_text=help_text, verbose_name=verbose_name
         )
         self.fk_resource = None
 
@@ -689,11 +689,11 @@ class ToManyField(RelatedField):
 
     def __init__(self, to, attribute, related_name=None, default=NOT_PROVIDED,
                  null=False, blank=False, readonly=False, full=False,
-                 unique=False, help_text=None):
+                 unique=False, help_text=None, verbose_name=None):
         super(ToManyField, self).__init__(
             to, attribute, related_name=related_name, default=default,
             null=null, blank=blank, readonly=readonly, full=full,
-            unique=unique, help_text=help_text
+            unique=unique, help_text=help_text, verbose_name=verbose_name
         )
         self.m2m_bundles = []
 
